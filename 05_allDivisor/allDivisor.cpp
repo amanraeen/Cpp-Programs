a47eeb8c57fa4fa2f2192756550a741b6b928a33
Mohammad Aman Malik <amankgn108@gmail.com>
2025-01-18 19:02:30 +0530

    print all Divisor program

---
+++ 05_allDivisor/allDivisor.cpp
@@ -0,0 +1,29 @@
+//program to print all divisiorsof a given numbers
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void printDivisor(int n){
+    vector<int> ls;
+    for(int i=1; i*i<=n; i++){
+       if(n%i==0){
+          ls.push_back(i);
+          if((n/i)!=i){
+            ls.push_back(n/i);
+        }
+       }
+
+     sort(ls.begin(), ls.end());
+     for(auto it : ls) cout<< it << " ";
+    } 
+}
+
+int main(){
+    int num;
+    cout<<"Enter Number : ";
+    cin>>num;
+
+    printDivisor(num);
+
+    return 0;
+}
